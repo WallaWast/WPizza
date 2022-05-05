52e6b8d55401e03bc8f827086d0eb3769e16818e
Wallace Torres <wallacewst@gmail.com>
2022-05-04 17:45:51 -0700

    Adding entityFramework to project

---
+++ WPizza/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPizza.Domain.Entities;
+
+namespace WPizza.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { }
+
+        public DbSet<Order> Orders => Set<Order>();
+    }
+}
