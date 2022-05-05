73f02a3b1ad6d10dca36285f1a5da0e04e3cdd7c
Wallace Torres <wallacewst@gmail.com>
2022-05-04 18:56:38 -0700

    Category data creation

--- WPizza/Data/DataContext.cs
+++ WPizza/Data/DataContext.cs
@@ -9,8 +9,33 @@ namespace WPizza.Data
         { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { }
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    Id = 1,
+                    Name = "Pizza"
+                }                ,
+                new Category()
+                {
+                    Id = 2,
+                    Name = "Vegetarian"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "Beverage"
+                },
+                new Category()
+                {
+                    Id = 4,
+                    Name = "Desserts"
+                }
+            );
+        }
 
         public DbSet<Order> Orders => Set<Order>();
+
+        public DbSet<Category> Categories => Set<Category>();
     }
 }
