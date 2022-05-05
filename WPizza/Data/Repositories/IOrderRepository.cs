dbb8d23f1e2bd29d6e44c93173562283585b8080
Wallace Torres <wallacewst@gmail.com>
2022-05-04 18:49:25 -0700

    Order repository creation

---
+++ WPizza/Data/Repositories/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using WPizza.Domain.Entities;
+
+namespace WPizza.Data.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task AddAsync(Order order);
+
+        Task UpdateAsync(Order order);
+
+        Task<List<Order>> GetAllOrdersAsync();
+
+        Task<Order?> GetOrderByIdAsync(int id);
+
+        Task DeleteAsync(Order order);
+    }
+}
---
+++ WPizza/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPizza.Domain.Entities;
+
+namespace WPizza.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DataContext _context;
+
+        public OrderRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.Entry(order).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Order>> GetAllOrdersAsync()
+        {
+            var orders = await _context.Orders.ToListAsync();
+
+            return orders;
+        }
+
+        public async Task<Order?> GetOrderByIdAsync(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            return order;
+        }
+
+        public async Task DeleteAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
