9149e67dbdfa5136911ca8b889869e0cf3e39777
Wallace Torres <wallacewst@gmail.com>
2022-05-04 17:33:42 -0700

    Creation of the entitie Order

---
+++ WPizza/Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WPizza.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalValue { get; set; }
+    }
+}
